ace6810f1dd7784977765615305fd505fcd19c0d
erasaninaveenui-oss <erasaninaveenui@gmail.com>
2026-05-29 13:17:06 +0530

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// FIX: The App component is now correctly imported as it is implemented in App.tsx.
 import App from './App';
+import './index.css';
 
 const rootElement = document.getElementById('root');
+
 if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+  throw new Error('Could not find root element to mount to');
 }
 
-const root = ReactDOM.createRoot(rootElement);
-root.render(
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
